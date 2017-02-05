1011ee61f47c61cbffd65ae05ca7d2bc86eb9fe5
pichsinee <p.pichsinee@gmail.com>
2017-02-05 15:12:29 +0700

    Finish Project Day2

--- app/src/main/java/comscisdu/alice/mytraffic/Detail.java
+++ app/src/main/java/comscisdu/alice/mytraffic/Detail.java
@@ -2,6 +2,9 @@ package comscisdu.alice.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class Detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView titleTextView = (TextView) findViewById(R.id.textView3);   //ผูก textView3 กับตัวแปร titleTextView
+        titleTextView.setText(getIntent().getStringExtra("Title")); //รับค่า Title จาก MainActivity (intent) มาแสดงที่  titleTextView
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView.setImageResource(getIntent().getIntExtra("Image",R.drawable.traffic_01));
+
+        TextView detailTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView.setText(getIntent().getStringExtra("Detail"));
+
+
+    }   //Main Method
+
+    public void clickBack(View view) {  //คำสั้ง View view เป็นคำสั่งที่ทำให้เห็นหน้า UI จากไฟล์ xml
+        finish();
     }
-}
+
+}   //Main Class
